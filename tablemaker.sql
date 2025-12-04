9dbfa37d65e4645526e0e73b6158ab14dea49090
Komail <komailraza448@gmail.com>
2025-12-05 03:24:08 +0500

    3

--- tablemaker.sql
+++ tablemaker.sql
@@ -1,21 +1,9 @@
-CREATE TABLE restaurant (
-    restaurant_id INT AUTO_INCREMENT PRIMARY KEY,
-
-    business_name VARCHAR(100) NOT NULL,
-    name_per_cnic VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-
-    business_type VARCHAR(100) NOT NULL,
-    business_category VARCHAR(100) NOT NULL,
-    business_update VARCHAR(100) NOT NULL,
-
-    email VARCHAR(120) NOT NULL UNIQUE,
-    phone VARCHAR(20) NOT NULL UNIQUE,
-
-    password_hash VARCHAR(255) NOT NULL,
-
-    restaurant_location INT NULL,
-    otp INT NULL,   
-
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE category (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    restaurant_id INT NOT NULL,
+    category_name VARCHAR(100) NOT NULL,
+    category_image VARCHAR(255),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
