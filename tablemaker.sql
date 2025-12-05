12728cf431bb4b3e503072aedecc34caa18a56db
Komail <komailraza448@gmail.com>
2025-12-06 02:20:10 +0500

    3

--- tablemaker.sql
+++ tablemaker.sql
@@ -1,5 +1,8 @@
 ALTER TABLE restaurant
-DROP COLUMN restaurant_location,
-ADD latitude DECIMAL(10,7),
-ADD longitude DECIMAL(10,7),
-ADD address VARCHAR(255);
+ADD COLUMN restaurant_image VARCHAR(500) NULL AFTER address;
+
+ALTER TABLE restaurant
+ADD COLUMN discount VARCHAR(100) NULL AFTER restaurant_image;
+
+ALTER TABLE restaurant
+ADD COLUMN rating DECIMAL(2,1) DEFAULT 4.0 AFTER discount;
